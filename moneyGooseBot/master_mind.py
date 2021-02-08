a4a1daa25e9c2f2a5bb3ccb827ac20a9985cb541
isbobby <bobbyclex8@gmail.com>
2021-02-08 21:48:22 +0800

    Made local debug changes

--- moneyGooseBot/master_mind.py
+++ moneyGooseBot/master_mind.py
@@ -10,10 +10,17 @@ def mainCommandHandler(incoming_message, telebot_instance):
     """Echo the user message."""
     chat_id = incoming_message.chat.id
     msg_id = incoming_message.message_id
-    user = incoming_message.from_user 
+    user = incoming_message.from_user
+
     try:
-        print(incoming_message.text)
+        # print(incoming_message.text)
         text = incoming_message.text.encode('utf-8').decode()
+        if (text == "benny"):
+            telebot_instance.sendMessage(chat_id=chat_id, text="Benny is gay", reply_to_message_id=msg_id)
+            return
+        elif (text == "shem"):
+            telebot_instance.sendMessage(chat_id=chat_id, text="Shem is Limos", reply_to_message_id=msg_id)
+            return
     except:
         print("no text in the message")
     try:
@@ -26,12 +33,15 @@ def mainCommandHandler(incoming_message, telebot_instance):
         telebot_instance.sendMessage(chat_id=chat_id, text="Hello I am goose", reply_to_message_id=msg_id)
     except:
         print("message is lost, bot has no target to reply")
-
     return 
 
 def start(update, context):
     """Send a message when the command /start is issued."""
-    update.message.reply_text('your mother fat!')
+    update.message.reply_text("User entered start")
+
+def stop(update, context):
+    """Callback when user enters /stop"""
+    update.message.reply_text("User entered stop")
 
 def error(update, context):
     """Log Errors caused by Updates."""
